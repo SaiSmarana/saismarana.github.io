b0b09f85086527c05c34a386fb9cc579b38b52c5
Saibaba Devotee <nifty.investments@gmail.com>
2024-01-13 19:11:16 -0600

    updating the index.html

---
+++ assets/js/header_loader.js
@@ -0,0 +1,20 @@
+// header_loader.js
+(function () {
+  // Get the current script location
+  const scriptElement = document.currentScript;
+
+  win_location_path = window.location.pathname; // Get the current path
+  const currentPath = win_location_path.endsWith('/') ? win_location_path : win_location_path.substring(0, win_location_path.lastIndexOf('/') + 1) ;
+
+  //console.log('The base path :' , basePath)
+  console.log('The current path :' , currentPath)
+ 
+  // Use fetch to load the content of snippet.html
+  fetch(`${currentPath}header.html`)
+  //fetch(`header.html`)
+    .then(response => response.text())
+    .then(html => {
+      const containerId = scriptElement.getAttribute('data-container-id');
+      document.getElementById(containerId).innerHTML = html;
+    });
+})();
